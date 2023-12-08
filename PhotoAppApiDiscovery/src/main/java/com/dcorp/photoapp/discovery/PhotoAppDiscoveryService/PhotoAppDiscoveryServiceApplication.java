6f98a94a5831c309e6ff61414e73c3f792c37d16
DeathLotu5 <duongph.4a@gmail.com>
2023-12-08 21:24:19 +0700

    [Add] - [DuongPH]: -adding discovery service

---
+++ PhotoAppApiDiscovery/src/main/java/com/dcorp/photoapp/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.dcorp.photoapp.discovery.PhotoAppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+	}
+
+}
