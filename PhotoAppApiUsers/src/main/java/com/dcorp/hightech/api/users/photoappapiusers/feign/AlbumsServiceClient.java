e2bb68c8b1042d5e94931cd27ca9a762da8bb9fd
DeathLotu5 <duongph.4a@gmail.com>
2023-12-20 22:20:01 +0700

    [Update] - [Duongph]: - Register and Login function

--- PhotoAppApiUsers/src/main/java/com/dcorp/hightech/api/users/photoappapiusers/feign/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/dcorp/hightech/api/users/photoappapiusers/feign/AlbumsServiceClient.java
@@ -1,16 +1,26 @@
 package com.dcorp.hightech.api.users.photoappapiusers.feign;
 
 import com.dcorp.hightech.api.users.photoappapiusers.controllers.ui.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallBack")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
 
+    default List<AlbumResponseModel> getAlbumsFallBack(String id, Throwable exception) {
+        System.out.println("Param = " + id);
+        System.out.println("Exception took place = " + exception.getMessage());
+        return new ArrayList<>();
+    }
 }
+
+
